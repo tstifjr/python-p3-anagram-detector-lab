c40bd536741b1ed5bdc4cd74d3a5eb61e2846ede
TJ Stifter <tjstifter@gmail.com>
2023-07-05 17:05:40 -0400

    completes lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,18 @@
 # your code goes here!
+
+class Anagram:
+    def __init__(self, word) :
+        self.word = word
+
+    def match (self, list) :
+        word_array = [letter for letter in self.word]
+        sorted_word_arr = sorted(word_array)
+        anagram_arr = []
+
+        for anagram in list :
+            an_arr = [letter for letter in anagram]
+            sorted_an_arr = sorted(an_arr)
+            if (sorted_an_arr == sorted_word_arr) :
+                anagram_arr.append(anagram)
+
+        return anagram_arr
